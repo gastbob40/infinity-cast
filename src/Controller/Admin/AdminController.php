9c6bfa40d2c723bfa76b58e9485173c7b690c02d
gastbob40 <gastbob40@gmail.com>
2020-12-07 11:58:25 +0100

    feat (admin): add admin link

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -6,10 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/admin", name="admin_")
+ */
 class AdminController extends AbstractController
 {
     /**
-     * @Route("/admin", name="admin")
+     * @Route("/", name="home")
      */
     public function index(): Response
     {
